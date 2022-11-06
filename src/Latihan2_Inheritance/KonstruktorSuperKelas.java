ca4bfb6b80062256693179949e3b9c24fadbedd6
AhmadReginaldSyahiran <112646010+AhmadReginaldSyahiran@users.noreply.github.com>
2022-11-06 13:46:16 +0700

    Delete src directory

--- src/Latihan2_Inheritance/KonstruktorSuperKelas.java
+++
@@ -1,8 +0,0 @@
-package Latihan2_Inheritance;
-
-public class KonstruktorSuperKelas {
-    public static void main(String[] args) {
-        Employ programer1 = new Employ("12345678", "budi", 33);
-        programer1.info();
-    }
-}
